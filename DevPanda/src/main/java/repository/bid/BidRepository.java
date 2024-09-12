58876b07e745c3c9f4c6388d7db406a8f5133a61
'ellie' <'ellie.sy1992@gmail.com'>
2024-09-12 14:02:38 +0900

    add moreBtn and companyBid fail and now

--- DevPanda/src/main/java/repository/bid/BidRepository.java
+++ DevPanda/src/main/java/repository/bid/BidRepository.java
@@ -12,11 +12,12 @@ public interface BidRepository {
 
 	Integer selectBuyBidCnt(HashMap<String, Object> pageInfo) throws Exception;
 
-	List<Map> selectBuyBidList(Integer row, String id) throws Exception;
-
-	List<Map> selectBuyBidWithCalDate(Integer row, String id, String nowStr, String pastDateStr) throws Exception;
+	List<Map> selectBuyBidList(Integer row, String id,String memType) throws Exception;
 
 	void updateBidState(String id) throws Exception;
 
 	List<Bid> selectAllBuyer(Integer auctionNum) throws Exception;
+
+	List<Map> selectBuyBidWithCalDate(Integer row, String id, String nowStr, String pastDateStr, String memType)
+			throws Exception;
 }
