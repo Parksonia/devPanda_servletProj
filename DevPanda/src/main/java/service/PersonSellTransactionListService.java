a81c9da265096283dfdfa060c9a9c94a3b030124
rena <rena1010dev@gmail.com>
2024-09-13 09:54:13 +0900

    .

--- DevPanda/src/main/java/service/PersonSellTransactionListService.java
+++ DevPanda/src/main/java/service/PersonSellTransactionListService.java
@@ -8,5 +8,5 @@ public interface PersonSellTransactionListService {
 	List<Map> personSellTransactionList(String sellerId) throws Exception;
 	List<Map> personSellTransactionListByDate(String sellerId, String filterDate) throws Exception;
 	List<Map> personSellTransactionListByDateRange(String sellerId, String startDate, String endDate) throws Exception;
-
+	
 }
