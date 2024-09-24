10197a7b759375f9aa0014ba8dcd3b8bca91bd80
KIJ9801 <154866994+KIJ9801@users.noreply.github.com>
2024-09-24 14:04:55 +0900

    TransactionInsert

--- DevPanda/src/main/java/repository/auction/AuctionSchedulerRepository.java
+++ DevPanda/src/main/java/repository/auction/AuctionSchedulerRepository.java
@@ -1,8 +1,21 @@
 package repository.auction;
 
+import java.util.List;
+
+import dto.Transaction;
+import dto.Bid;
+
 public interface AuctionSchedulerRepository {
     void updateAuctionStatusToSuccess(String currentDate);
     void updateBidStateToTwo(String currentDate);
+    void updateWinningBidState(int auctionNum);
+    void updateLosingBiddersState(int auctionNum);
+    
+    List<Integer> getFinishedAuctions(String currentDate);
+    
+    void insertTransaction(Bid bid); 
+	List<Bid> getWinningBids(Integer auctionNum);
 
+  
 
 }
